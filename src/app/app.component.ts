ef8b37b8413c976569e704ce0d6af54c6bf76c15
Venkat Narasimha Rao Golagani <38693729+venkatnarasimharao@users.noreply.github.com>
2023-06-02 11:43:09 +0530

    gtm partytown changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+declare global {
+  interface Window {
+    dataLayer: any;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'gtmPartytown';
+
+  postGtm() {
+    if (window && window['dataLayer']) {
+      window.dataLayer.push({
+        event: 'buttonClicked',
+        buttonName: 'MyButton',
+        // Additional data you want to pass to GTM
+      });
+      console.log(window['dataLayer'], 'check')
+    }
+  }
 }
